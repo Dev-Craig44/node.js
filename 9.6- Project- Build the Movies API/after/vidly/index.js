0ff4508ac46404a4a8c81e611c1d202c7daab509
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-10-15 00:13:57 -0400

    🧠 Clean up index.js

--- 9.6- Project- Build the Movies API/after/vidly/index.js
+++ 9.6- Project- Build the Movies API/after/vidly/index.js
@@ -1,16 +1,12 @@
-require("express-async-errors");
-
 const express = require("express");
 const app = express();
 
 // 7.) Import the logging startup file and before the routes and db startup files so that if there are any errors in those files, they will be logged.
-require("./startup/logging");
+const logger = require("./startup/logging");
 require("./startup/config");
 require("./startup/routes")(app);
 require("./startup/db")();
 
-const p = Promise.reject(new Error("Something failed miserably!"));
-p.then(() => console.log("Done"));
-
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+// Use the logger to log that the app is listening on the port
+app.listen(port, () => logger.info(`Listening on port ${port}...`));
